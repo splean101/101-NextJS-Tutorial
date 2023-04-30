b97b8fc5ea9a8aaa35534732972119e2f0f11240
splean101 <101splean@gmail.com>
2023-04-30 15:01:48 +0300

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,16 @@
 import Heading from "@/components/heading";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Error = () => {
+const router = useRouter()
+
+useEffect(()=>{
+  setTimeout(()=>{
+router.push('/')
+  }, 3000)
+},[router])
+
   return (
     <>
       <Heading text="404" tag="h1" />
